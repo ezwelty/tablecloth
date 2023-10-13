09442d93366145a77dd86e7d0134ba8fa7406a27
ezwelty <ethan.welty@gmail.com>
2023-10-13 18:50:25 +0200

    Add source code to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -9,6 +9,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx_autodoc_typehints',
+    'sphinx.ext.viewcode',
 ]
 html_theme = 'sphinx_rtd_theme'
 autodoc_member_order = 'bysource'
